015d7798773bf399515ba44a55b565b4a17428fe
Mehroz-Arif <122996715+Mehroz-Arif@users.noreply.github.com>
2024-03-04 23:28:07 +0500

    My first Page

---
+++ javascript.js
@@ -0,0 +1,40 @@
+//let elemclass= document.getElementsByClassName("nav-logo");
+  //  elemclass[0].classList.add("bg-primary");
+    //let elem=document.getElementById('home-section')
+    //createdelement=document.createElement('p')
+    //createdelement.innertext="this is the para"
+    //tn = document.getElementsByTagName('div')
+    //console.log(tn)
+    //tn[2].appendChild(createdelement)
+    //sel= document.querySelectorAll('.nav-logo')
+    //console.log(sel);
+
+    //events in javascript
+  //function clicked()
+  //{
+
+   //let cl= document.getElementsByClassName('container-btn').style.boder='10px solid green'
+    //  console.log("this button was clicked")
+  //}
+      //window.onload = function()
+    //  {
+      //  console.log("this document was loaded")
+
+      //}
+     //container-btn.addEventListener('click', function(){
+       // console.log("container clicked")
+      //})
+
+// arrow function
+
+//sum=(a,b)=>{
+  //  return a+b;
+//}
+//logkaro=()=>{
+    //document.querySelectorAll('services-card-container')[0].innerHTML=" set interval fired "
+   //let aa = document.querySelectorAll('services-card-container')[1]
+  // console.log(aa)
+    //console.log("i am mehroz")
+//}
+//set time out
+//setTimeout(logkaro,2000);
